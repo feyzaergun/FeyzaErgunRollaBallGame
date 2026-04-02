a4a9706d004e237ea9088580175a11d28055ad3f
ergunfeyzaergun <240404015@ogr.kent.edu.tr>
2026-04-02 22:47:18 +0300

    Bonus
    Standard cube collectibles have been replaced with 'Magic Bottle' assets. Applied custom materials and lighting to enhance the visual environment. Added a black skybox material.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,70 +1,64 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
-using TMPro; // Ekrana yazı yazdırmak için bu şart!
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+    // Hareket Ayarları
     public float speed = 10f;
-
-    // UI Yazı Elementleri
-    public TextMeshProUGUI countText; // Skor yazısı
-    public TextMeshProUGUI winTextObject; // "You Win!" yazısı (Son fotoğraftaki yenilik)
-
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+
+    // UI Ayarları
+    public TextMeshProUGUI countText;
+    public GameObject winObject;
     private int count;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        // Başlangıç değerleri
         count = 0;
-
         SetCountText();
-
-        // --- YENİ: Oyun başlarken "You Win!" yazısını gizle ---
-        winTextObject.gameObject.SetActive(false);
+        winObject.SetActive(false);
     }
 
+    // Yeni Input System hareketi
     void OnMove(InputValue movementValue)
     {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        Vector2 v = movementValue.Get<Vector2>();
+        movementX = v.x;
+        movementY = v.y;
     }
 
     void FixedUpdate()
     {
+        // Karakteri hareket ettir
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
     }
 
+    // İksirleri (Magic Tubes) toplama
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
             count = count + 1;
-
-            SetCountText(); // Skor her arttığında kontrol et
+            SetCountText();
         }
     }
 
-    // Skoru güncelleyen ve oyunun bitip bitmediğini kontrol eden fonksiyon
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
 
-        // --- YENİ: Kazanma kontrolü (Kritik Nokta!) ---
-        // Sahnede kaç tane altın (PickUp) varsa buradaki sayıyı ona göre değiştir.
-        // Ben bir önceki fotoğrafına bakarak 8 tane saydım, o yüzden 8 yazıyorum.
+        // 8 tane toplayınca kazandın yazısı çıksın
         if (count >= 8)
         {
-            // Tüm altınlar toplanınca "You Win!" yazısını göster
-            winTextObject.gameObject.SetActive(true);
+            winObject.SetActive(true);
         }
     }
 }
